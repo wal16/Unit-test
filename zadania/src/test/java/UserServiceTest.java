b8176c55c23d2531ee869a5664edeca631d80fb0
waldemar <waldemar.mozola@onet.pl>
2018-02-16 03:20:35 +0100

    init

--- zadania/src/test/java/UserServiceTest.java
+++ zadania/src/test/java/UserServiceTest.java
@@ -1,134 +1,119 @@
-
 import com.infoshareacademy.javaintroduction.datastructures.task7.domain.*;
 import com.infoshareacademy.javaintroduction.datastructures.task7.service.UserService;
 import com.infoshareacademy.javaintroduction.datastructures.task7.domain.Permission;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.util.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class UserServiceTest {
+    @Test
+    public void findUsersWhoHaveMoreThanOneAddress() {
+        List<User> users = new ArrayList<>();
+        List<Address> addresses = new ArrayList<>();
+        users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList(new Address(), new Address(), new Address())))));
+        users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList(new Address(), new Address(), new Address())))));
+        users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList(new Address(), new Address(), new Address(), new Address())))));
+        users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList(new Address(), new Address(), new Address(), new Address())))));
+        assertThat(UserService.findUsersWhoHaveMoreThanOneAddress(users)).hasSize(4);
+    }
 
+    @Test
+    public void findOldestPerson() {
+        //given
+        User user1 = new User();
+        Person person1 = new Person();
+        person1.setAge(18);
+        user1.setPersonDetails(person1);
+        User user2 = new User();
+        Person person2 = new Person();
+        person2.setAge(50);
+        user2.setPersonDetails((person2));
+        //when
+        Person oldestPerson = UserService.findOldestPerson(Arrays.asList(user1, user2));
+        //then
+        assertThat(oldestPerson).isEqualTo(person2);
+        assertEquals("oldest person", person2, oldestPerson);
+    }
 
+    @Test
+    public void shouldFindUserWithLongestUsername() {
+        List<User> users = new ArrayList<>();
+        users.add(new User().setName("Tesla"));
+        users.add(new User().setName("Edison"));
+        users.add(new User().setName("Copernicus"));
+        assertThat(UserService.findUserWithLongestUsername(users).getName()).isEqualToIgnoringCase("Copernicus");
+    }
 
-        @Test
-        public void findUsersWhoHaveMoreThanOneAddress() {
+    @Test
+    public void findUserWithLongestUsername() {
+        List<User> users = new ArrayList<>();
+        users.add(new User().setName("Waldek"));
+        users.add(new User().setName("Ryszard"));
+        users.add(new User().setName("Zbigniew"));
+        assertThat(UserService.findUserWithLongestUsername(users).getName()).containsIgnoringCase("Zbigniew");
+    }
 
-                List<User> users = new ArrayList<> ();
-                List<Address> addresses = new ArrayList<>();
-                users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList (new Address(), new Address())))));
-                users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList (new Address())))));
-                users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList (new Address(), new Address(), new Address())))));
-                users.add(new User().setPersonDetails(new Person().setAddresses(new ArrayList<Address>(Arrays.asList (new Address(), new Address(), new Address())))));
+    @Test
+    public void getNamesAndSurnamesCommaSeparatedOfAllUsersAbove18() {
+        User user1 = new User();
+        Person personDetails1 = new Person();
+        personDetails1.setAge(50);
+        personDetails1.setName("Aleksander");
+        personDetails1.setSurname("Kwaśniewski");
+        User user2 = new User();
+        Person personDetails2 = new Person();
+        personDetails2.setAge(9);
+        personDetails2.setName("Natalia");
+        personDetails2.setSurname("Zwara");
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        assertEquals("user above 18", 50, personDetails1.getAge());
+        assertEquals("user under 18", 9, personDetails2.getAge());
+    }
 
-              assertThat(UserService.findUsersWhoHaveMoreThanOneAddress(users)).hasSize(3);
+    @Test
+    public void groupUsersByRole() {
+        //given
+        User user1 = new User();
+        User user2 = new User();
+        Role rola1 = new Role();
+        rola1.setName("rola1");
+        Person personDetails1 = new Person();
+        personDetails1.setRole(rola1);
+        user1.setPersonDetails(personDetails1);
+        Role rola2 = new Role();
+        rola2.setName("rola2");
+        Person personDetails2 = new Person();
+        personDetails2.setRole(rola2);
+        user2.setPersonDetails(personDetails2);
+        //when
+        Map<Role, List<User>> role = UserService.groupUsersByRole(Arrays.asList(user1, user2));
+        //then
+        assertThat(role).isNotEmpty();
+        assertThat(role).containsKey(rola1);
+    }
 
-        }
-
-        @Test
-        public void findOldestPersonTest() {
-
-                User user1 = new User();
-                Person person1 = new Person();
-                person1.setAge(14);
-                user1.setPersonDetails(person1);
-
-                User user2 = new User();
-                Person person2 = new Person();
-                person2.setAge(18);
-                user2.setPersonDetails(person2);
-
-               List<User> users = new ArrayList<> ();
-                users.add(user1);
-                users.add(user2);
-
-               Person oldestPerson = UserService.findOldestPerson(users);
-
-                assertEquals("oldest person", person2, oldestPerson);
-                assertThat(UserService.findOldestPerson(users).getAge()).isEqualTo(18);
-
-        }
-
-        @Test
-        public void findOldestPersonTest2() {
-                List<User> users = new ArrayList<> ();
-
-                users.add(new User().setPersonDetails(new Person().setAge(20)));
-                users.add(new User().setPersonDetails(new Person().setAge(40)));
-
-                assertThat(UserService.findOldestPerson(users).getAge()).isEqualTo(40);
-
-        }
-
-
-        @Test
-        public void findUserWithLongestUsernameTest() {
-
-                User user1 = new User();
-                user1.setName("Kowalski");
-
-                User user2 = new User();
-                user2.setName("Rak");
-
-                List<User> users = new ArrayList<> ();
-                users.add(user1);
-                users.add(user2);
-
-                User longestUserName = UserService.findUserWithLongestUsername(users);
-
-                assertEquals("longestUserName", user1, longestUserName);
-        }
-
-        @Test
-        public void findUserWithLongestUsernameTest2() {
-                List<User> users = new ArrayList<> ();
-                users.add(new User().setName("Tomasz"));
-                users.add(new User().setName("Tom"));
-                users.add(new User().setName("Hermenegilda"));
-
-                assertThat(UserService.findUserWithLongestUsername(users).getName()).containsIgnoringCase("Hermenegilda");
-        }
-
-
-        @Test
-        public void getNamesAndSurnamesCommaSeparatedOfAllUsersAbove18Test() {
-
-
-                User user1 = new User();
-                Person personDetails1 = new Person();
-                personDetails1.setAge(19);
-                personDetails1.setName("Marek");
-                personDetails1.setSurname("Sowa");
-
-                User user2 = new User();
-                Person personDetails2 = new Person();
-                personDetails2.setAge(11);
-                personDetails2.setName("Aga");
-                personDetails2.setSurname("Sowa");
-
-                List<User> users = new ArrayList<> ();
-                users.add(user1);
-                users.add(user2);
-
-                assertEquals("user above 18", 19,  personDetails1.getAge());
-                assertEquals("user under 18", 11, personDetails2.getAge());
-                }
-
-
-        @Test
-        public void getSortedPermissionsOfUsersWithNameStartingWithATest() {
-
-                List<User> users = new ArrayList<>();
-
-                users.add(new User().setName("Ala").setPersonDetails(new Person().setRole(new Role()
-                        .setPermissions(new ArrayList<>(Arrays.asList(new Permission().setName("p1"), new Permission().setName("p2")))))));
-
-                users.add(new User().setName("Ania").setPersonDetails(new Person().setRole(new Role()
-                        .setPermissions(new ArrayList<>(Arrays.asList(new Permission().setName("p3")))))));
-                users.add(new User().setName("Tom").setPersonDetails(new Person()
-                        .setRole(new Role().setPermissions(new ArrayList<>(Arrays.asList(new Permission().setName("p4")))))));
-
-                assertThat(UserService.getSortedPermissionsOfUsersWithNameStartingWithA(users)).containsOnly("p1", "p2", "p3");
-
-        }
+    @Test
+    public void partitionUserByUnderAndOver18() {
+        //given
+        User user1 = new User();
+        Person person1 = new Person();
+        person1.setAge(11);
+        user1.setPersonDetails(person1);
+        User user2 = new User();
+        Person person2 = new Person();
+        person2.setAge(55);
+        user2.setPersonDetails((person2));
+        //when
+        Map<Boolean, List<User>> partitionedByAge = UserService.partitionUserByUnderAndOver18(Arrays.asList(user1, user2));
+        //then
+        assertThat(partitionedByAge).isNotNull();
+        assertThat(partitionedByAge).containsKeys(true, false);
+        assertThat(partitionedByAge.get(false)).contains(user1);
+    }
 }
